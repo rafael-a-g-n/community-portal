12f7cccda020c7402c47c7e7ddf742207f9c2dba
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2026-04-16 09:43:54 +0000

    config(db): postgres settings via env vars

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -1,4 +1,5 @@
 """Django settings for core project."""
+import os
 from pathlib import Path
 
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -54,8 +55,12 @@ ASGI_APPLICATION = "core.asgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASSWORD"),
+        "HOST": os.getenv("DB_HOST"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
