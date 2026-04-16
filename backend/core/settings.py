92dfce78f355ad46f7ced6ca4754d308d2f376c9
Rafael Alexandre Guerreiro Nogueira <124052359+rafael-a-g-n@users.noreply.github.com>
2026-04-16 10:47:50 +0000

    feat(reports): create reports Django app

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -16,6 +16,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "reports.apps.ReportsConfig",
     "rest_framework",
     "django_filters",
     "drf_spectacular",
